e18b0e8b12123bd560d14c1f2acf7df4f76f5d05
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-09-26 16:17:10 -0500

    chore: avance class Productomodel

--- libreria/gestion/models.py
+++ libreria/gestion/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+#https://docs.djangoproject.com/en/3.2/ref/models/
+#https://docs.djangoproject.com/en/3.2/topics/db/models/
+
+class ProductoModel(models.Model):
+    # Tipos de datos del ORM => https://docs.djangoproject.com/en/3.2/ref/models/fields/#field-types
+    # Parametros genericos de lo tipos de datos => https://docs.djangoproject.com/en/3.2/ref/models/fields/#field-options
+    productoId = models.AutoField(
+        primary_key=True, null=False, unique=True, db_column='id')
+
+    productoNombre = models.CharField(
+      max_length=45, db_column='nombre', null=False)  
+    
