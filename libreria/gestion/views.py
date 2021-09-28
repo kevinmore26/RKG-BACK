b4b4b116fc5a68a9324c3b6a02b81bce300082d0
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-09-28 15:28:05 -0500

    chore: agregue urls

--- libreria/gestion/views.py
+++ libreria/gestion/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+class PruebaController(APIView):
+    def get(self, request):
+        return {'message': 'Exito'}
+
--- libreria/libreria/settings.py
+++ libreria/libreria/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Lima'
 
