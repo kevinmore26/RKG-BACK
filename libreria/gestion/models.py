e29d496bf5250d40cc9a88fb67a7f52e76b7b41a
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-09-29 15:24:13 -0500

    arreglos

--- libreria/gestion/models.py
+++ libreria/gestion/models.py
@@ -1,7 +1,8 @@
-from django.db.models.deletion import CASCADE
+# from django.db.models.deletion import CASCADE
+# from libreria import gestion
 from django.db import models
 
-
+# Create your models here.
 
 class PerfilModel(models.Model):
     perfilId = models.AutoField(
@@ -27,7 +28,15 @@ class ClienteModel(models.Model):
     perfilCliente = models.CharField(
          db_column='PerfilModel',max_length=19
     )
-    
+    perfiles = models.ForeignKey(to=PerfilModel, db_column='perfiles_id',
+                                 null=False, related_name='perfilCliente0', on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'clientes'
+        verbose_name = 'cliente'
+        verbose_name_plural = 'clientes'
+
+
 class ProductoModel(models.Model):
 
     class OpcionesUM(models.TextChoices):
@@ -56,47 +65,87 @@ class ProductoModel(models.Model):
         ordering = ['-productoPrecio']
         verbose_name = 'producto'
         verbose_name_plural = 'productos'
-      
-class OrdenCabeceraModel(models.Model):
+
+
+
+# ORDEN_CABECERA
+class CabeceraModel(models.Model):
     # tipos = VENTA V | COMPRA C
     class OpcionesTipo(models.TextChoices):
         VENTA = 'V', 'VENTA'
         COMPRA = 'C', 'COMPRA'
 
-    ordenCabeceraId = models.AutoField(
+    cabeceraId = models.AutoField(
         db_column='id', primary_key=True, unique=True, null=False)
 
-    ordenCabeceraFecha = models.DateTimeField(auto_now_add=True, db_column='fecha')
+    cabeceraFecha = models.DateTimeField(auto_now_add=True, db_column='fecha')
 
-    ordenCabeceraTipo = models.TextField(
+    cabeceraTipo = models.TextField(
         choices=OpcionesTipo.choices, db_column='tipo', null=False)
 
-    # ------------------------
+   
     clientes = models.ForeignKey(to=ClienteModel, db_column='clientes_id',
                                  null=False, related_name='clienteCabeceras', on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'cabecera_operaciones'
-        verbose_name = 'ordencabecera'
-        verbose_name_plural = 'ordencabeceras'
+        verbose_name = 'cabecera'
+        verbose_name_plural = 'cabeceras'
 
 
-class OrdenDetalleModel(models.Model):
-    ordenDetalleId = models.AutoField(
+class DetalleModel(models.Model):
+    detalleId = models.AutoField(
         db_column='id', primary_key=True, unique=True, null=False)
 
-    ordenDetalleCantidad = models.IntegerField(db_column='cantidad', null=False)
-    
-    ordenDetalleImporte = models.DecimalField(
+    detalleCantidad = models.IntegerField(db_column='cantidad', null=False)
+
+    detalleImporte = models.DecimalField(
         max_digits=5, decimal_places=2, db_column='importe', null=False)
-    # CAMBIAR RELATED_NAME
+
     productos = models.ForeignKey(to=ProductoModel, db_column='productos_id',
                                   on_delete=models.PROTECT, related_name='productoDetalles', null=False)
-    
-    cabeceras = models.ForeignKey(to=OrdenCabeceraModel, db_column='cabecera_operaciones_id',
+
+    cabeceras = models.ForeignKey(to=CabeceraModel, db_column='cabecera_operaciones_id',
                                   on_delete=models.PROTECT, related_name='cabeceraDetalles', null=False)
 
     class Meta:
         db_table = 'detalle_operaciones'
         verbose_name = 'detalle'
         verbose_name_plural = 'detalles'
+
+
+class AdopcionModel(models.Model):
+
+    class OpcionesUM(models.TextChoices):
+        PEQUEÑO = 'P', 'PEQUEÑO' 
+        MEDIANO = 'M', 'MEDIANO' 
+        GRANDE = 'G', 'GRANDE' 
+    adopcionId = models.AutoField(
+        primary_key=True, null=False, unique=True, db_column='id')
+
+    adopcionNombre = models.CharField(
+      db_column='nombre', null=False, max_length=14)
+
+    adopcionEdad = models.IntegerField(null=True,unique=True,db_column='edad')
+
+    adopcionTamaño = models.TextField(
+        choices=OpcionesUM.choices, default=OpcionesUM.MEDIANO, db_column='tamaño')
+    
+    adopcionCaracteristicas = models.CharField(
+      db_column='caracteristicas', null=False, max_length=100)
+    
+
+    # place = models.OneToOneField(
+    #     Place,
+    #     on_delete=models.CASCADE,
+    #     primary_key=True,
+    # )
+    clientes = models.OneToOneField(to=ClienteModel,db_column='cliente_id',
+                                  on_delete=models.CASCADE,related_name='clienteAdopcion', null=False)
+
+    class Meta:
+       
+        db_table='adopciones'
+        ordering = ['-adopcionTamaño']
+        verbose_name = 'adopcion'
+        verbose_name_plural = 'adopciones'
