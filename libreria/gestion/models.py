93737e6f02eec905012cfe6654272a3098ee9fa4
Guillermo Nicanor Mujica Davila <85207817+GuillermoMujicaDavila@users.noreply.github.com>
2021-09-28 22:47:36 -0500

    Avance1
    Se creo los modelos de las tablas

--- libreria/gestion/models.py
+++ libreria/gestion/models.py
@@ -1,3 +1,31 @@
+from django.db.models.deletion import CASCADE
+from libreria import gestion
 from django.db import models
 
 # Create your models here.
+
+class PerfilModel(models.Model):
+    perfilId = models.AutoField(
+        primary_key=True,null=False,unique=True,db_column='id'
+    )
+    perfilNick = models.CharField(
+        max_length=45,db_column='nick'
+    )
+
+class ClienteModel(models.Model):
+    clienteId = models.AutoField(
+        primary_key=True, null= False, unique= True, db_column='id'
+    )
+    clienteNombre = models.CharField(
+        max_length=45, db_column='nombre'
+    )
+    clienteDocumento = models.CharField(
+        max_length=45, db_column='documento'
+    )
+    clienteCelular = models.AutoField(
+        primary_key=False, null=True,unique=True,db_column='celular'
+    )
+    # perfilCliente = models.ForeignKey(
+    #      'PerfilModel', on_delete=models.CASCADE
+    # )
+    
