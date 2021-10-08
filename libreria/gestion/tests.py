d41d9a23c7da79f881ebbe1a1a5fd95a5010b9d1
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-10-08 17:17:03 -0500

    chore: test 2

--- libreria/gestion/tests.py
+++ libreria/gestion/tests.py
@@ -1,10 +1,17 @@
-from rest_framework import request
 from rest_framework.test import APITestCase
+from .models import ProductoModel
 
 class ProductosTestCase(APITestCase):
+
+    def setUp(self):
+        ProductoModel(  productoNombre= 'Casa de perro',
+                        productoPrecio= 120.90,
+                        productoFoto= 'casaperro.jpg',
+                        productoDescripcion= 'Casa para perro').save()
+
     def test_post_fail(self):
         '''Deberia fallar el test cuando no le pasamos la informacion'''
-        print(self.shortDescription())
+        # print(self.shortDescription())
 
         request = self.client.post('/gestion/productos/')
         message = request.data.get('message')
@@ -14,7 +21,7 @@ class ProductosTestCase(APITestCase):
     
     def test_post_success(self):
         '''Deberia retornar el producto creado'''
-        print(self.shortDescription())
+        # print(self.shortDescription())
 
         request = self.client.post('/gestion/productos/', data={
             "productoNombre": "Casa de perro",
@@ -23,9 +30,20 @@ class ProductosTestCase(APITestCase):
             "productoDescripcion": "Casa para perro2"
         }, format='json')
         message = request.data.get('message')
+        id = request.data.get('content').get('productoId')
+        print(id)
+        productoEncontrado = ProductoModel.objects.filter(
+            productoId=id).first()
 
         self.assertEqual(request.status_code, 201)
         self.assertEqual(message, 'Producto creado exitosamente')
+        self.assertIsNotNone(productoEncontrado)
 
 
-    
+    def test_get_success(self):
+        '''Deberia retornar los productos almacenados'''
+
+        productoEncontrado = ProductoModel.objects.all()
+        print(productoEncontrado)
+
+        self.assertEqual(1, 1)
