83e11ec57ce406c1201a220fad13d6ad6cd53b0d
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-10-09 00:15:16 -0500

    chore: test 3

--- libreria/gestion/tests.py
+++ libreria/gestion/tests.py
@@ -4,10 +4,24 @@ from .models import ProductoModel
 class ProductosTestCase(APITestCase):
 
     def setUp(self):
-        ProductoModel(  productoNombre= 'Casa de perro',
+        ProductoModel(  
+                        productoNombre= 'Casa de perro1',
+                        productoId=4,
                         productoPrecio= 120.90,
-                        productoFoto= 'casaperro.jpg',
-                        productoDescripcion= 'Casa para perro').save()
+                        productoFoto= 'casaperro1.jpg',
+                        productoDescripcion= 'Casa para perro1').save()
+        ProductoModel(  
+                        productoNombre= 'Casa de perro2',
+                        productoId=2,
+                        productoPrecio= 120.90,
+                        productoFoto= 'casaperro2.jpg',
+                        productoDescripcion= 'Casa para perro2').save()
+        ProductoModel(  
+                        productoNombre= 'Casa de perro3',
+                        productoId=3,
+                        productoPrecio= 120.90,
+                        productoFoto= 'casaperro3.jpg',
+                        productoDescripcion= 'Casa para perro3').save()
 
     def test_post_fail(self):
         '''Deberia fallar el test cuando no le pasamos la informacion'''
@@ -24,6 +38,7 @@ class ProductosTestCase(APITestCase):
         # print(self.shortDescription())
 
         request = self.client.post('/gestion/productos/', data={
+            "productoId": 65,
             "productoNombre": "Casa de perro",
             "productoPrecio": 120.90,
             "productoFoto": "casaperro.jpg",
@@ -47,3 +62,15 @@ class ProductosTestCase(APITestCase):
         print(productoEncontrado)
 
         self.assertEqual(1, 1)
+
+    def test_get_id_success(self):
+        '''Deberia retornar los productos almacenados por id'''
+
+        productoEncontrado = ProductoModel.objects.all()
+        # print(productoEncontrado)
+        request = self.client.get('/gestion/producto/2')
+
+        print(request.data)     
+        self.assertEqual(1, 1)
+          
+
